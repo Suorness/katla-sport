ba151e2b1488fcfb23d66ffdbdfd377165bb68c7
Vasya Dementey <dementey.vasya@gmail.com>
2018-06-16 22:14:21 +0300

    Add hive-section-list-item model. Implement method getHiveSection in hive.service. Implement interface OnInit and OnDestroy for HiveSectionComponent. Create view for HiveSectionListComponent.

--- KatlaSportNg/src/app/hive-management/lists/hive-section-list.component.ts
+++ KatlaSportNg/src/app/hive-management/lists/hive-section-list.component.ts
@@ -1,17 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { HiveSectionListItem } from '../models/hive-section-list-item';
 import { HiveService } from '../services/hive.service';
+import {Subscription} from "rxjs/index";
 
 @Component({
   selector: 'app-hive-section-list',
   templateUrl: './hive-section-list.component.html',
   styleUrls: ['./hive-section-list.component.css']
 })
-export class HiveSectionListComponent implements OnInit {
+export class HiveSectionListComponent implements OnInit, OnDestroy {
 
   hiveId: number;
   hiveSections: Array<HiveSectionListItem>;
+  sub1: Subscription;
 
   constructor(
     private route: ActivatedRoute,
@@ -20,5 +22,15 @@ export class HiveSectionListComponent implements OnInit {
   ) { }
 
   ngOnInit() {
+    this.sub1 = this.route.params.subscribe(p => {
+      this.hiveId = p['id'];
+      this.hiveService.getHiveSections(this.hiveId).subscribe(s => this.hiveSections = s);
+    })
+  }
+
+  ngOnDestroy() {
+    if (this.sub1){
+      this.sub1.unsubscribe();
+    }
   }
 }
