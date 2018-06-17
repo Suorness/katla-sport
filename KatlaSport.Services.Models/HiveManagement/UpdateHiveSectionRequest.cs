c4c571a95b54f8bce4d67ee716d0315ba946c53c
Vasya Dementey <dementey.vasya@gmail.com>
2018-06-17 22:07:39 +0300

    Merge step13. In interface IHiveSectionService added methods for adding, modifying and deleting. Implemented methods for adding, modifying and deleting in HiveSectionService. Add new maps. Added a new data model (UpdateHiveSectionRequest) and its validation (UpdateHiveSectionRequestValidator). Added to the HiveSectionController added, modified and deleted methods.

---
+++ KatlaSport.Services.Models/HiveManagement/UpdateHiveSectionRequest.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation.Attributes;
+
+namespace KatlaSport.Services.HiveManagement
+{
+    /// <summary>
+    /// Represents a request for creating and updating a Hive Section.
+    /// </summary>
+    [Validator(typeof(UpdateHiveSectionRequestValidator))]
+    public class UpdateHiveSectionRequest
+    {
+        /// <summary>
+        /// Gets or sets a product name.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets a product code.
+        /// </summary>
+        public string Code { get; set; }
+    }
+}
